f5316e68c56b9b194eae6ad9b11fdf0b25cbf68f
dhruv3 <dhruv.mnit@gmail.com>
2018-08-01 16:57:38 -0400

    Adding actions and setting up connection b/w react and redux.

--- tut-projects/ReduxSimpleStarter/src/containers/book-list.js
+++ tut-projects/ReduxSimpleStarter/src/containers/book-list.js
@@ -1,7 +1,9 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {selectBook} from '../actions/index';
+import {bindActionCreators} from 'redux';
 
-export default class BookList from Component{
-  
+class BookList extends Component{
   renderList(){
     return this.props.books.map((book) => {
       return(
@@ -18,3 +20,23 @@ export default class BookList from Component{
     )
   }
 }
+
+//Anything returned from this function will end up as this.props
+//on the BookList container
+function mapStateToProps(state){
+  //whatever is returned will show up as props
+  //inside this BookList
+  return{
+     books: state.books
+  }
+}
+
+function mapDispatchToProps(dispatch){
+  //Whenever selectBook is called, the result should be
+  //passed to all the reducers
+  return bindActionCreators({selectBook: selectBook}, dispatch)
+}
+
+//PRomote BookList from a component to container - it needs to know
+//about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
