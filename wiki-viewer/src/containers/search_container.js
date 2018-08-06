3e88adb667278eb8b547e47cc92beed4bbbdf824
dhruv3 <dhruv.mnit@gmail.com>
2018-08-06 18:18:42 -0400

    Adding styling and updating structure

--- wiki-viewer/src/containers/search_container.js
+++ wiki-viewer/src/containers/search_container.js
@@ -11,21 +11,25 @@ class SearchContainer extends Component{
     this.setState({term: event.target.value});
   }
   onFormSubmit(event){
-    
+
   }
   render(){
     return(
-      <form onSubmit={this.onFormSubmit.bind(this)} className="input-group">
-        <input
-          placeholder="Search Wikipedia..."
-          className="form-control"
-          value={this.state.term}
-          onChange={this.onInputChange.bind(this)}/>
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-primary">Submit</button>
-        </span>
-        <button className="btn btn-secondary">Clear</button>
-        <button className="btn btn-primary">Random!</button>
+      <form onSubmit={this.onFormSubmit.bind(this)} className="container searchFprm">
+        <div className="row margin-bottom-20 margin-top-20">
+          <input
+            placeholder="Search Wikipedia..."
+            className="col-sm-10"
+            value={this.state.term}
+            onChange={this.onInputChange.bind(this)}/>
+          <span className="col-sm-2">
+            <button type="submit" className="btn btn-primary">Submit</button>
+          </span>
+        </div>
+        <div className="row btnContainer">
+          <button className="btn btn-secondary margin-right-20">Clear</button>
+          <button className="btn btn-primary">Random!</button>
+        </div>
     </form>
     )
   }
