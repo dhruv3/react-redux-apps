2bcffa330728436703f7d5ff6e9670fee6356a1f
dhruv3 <dhruv.mnit@gmail.com>
2018-08-07 22:34:33 -0400

    Setting up file structure and initial naming of containers

--- recipe-box/src/components/App.js
+++ recipe-box/src/components/App.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import RecipeListContainer from '../containers/recipe_list';
+import RecipeDetailContainer from '../containers/recipe_detail';
+import RecipeEditModalContainer from '../containers/recipe_edit_modal';
 
 class App extends Component {
+
   render() {
     return (
-      <div>
+      <div className="container">
+        <RecipeListContainer/>
+        <RecipeDetailContainer/>
+        <RecipeEditModalContainer/>
       </div>
     );
   }
