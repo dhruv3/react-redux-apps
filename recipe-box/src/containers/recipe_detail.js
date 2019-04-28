db0a1aacb3843936f99a9142ebe4c76ee00c65f5
dhruv3 <dhruv.mnit@gmail.com>
2019-04-28 15:19:57 -0500

    adding content in recipe box

--- recipe-box/src/containers/recipe_detail.js
+++ recipe-box/src/containers/recipe_detail.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class RecipeDetail extends Component{
+  constructor(props){
+    super(props);
+    this.createBox = this.createBox.bind(this);
+  }
+  
+  createBox(elems){
+    let arr = [];
+    arr.push(elems.map(
+        (elem, index) => {
+            return (<li key={index}>
+              <div>{elem}</div>
+            </li>)
+        }
+      ))
+    return arr;
+  }
+
+  
   render(){
     return(
       <div className="row recipeDetailContainer">
@@ -14,6 +32,18 @@ class RecipeDetail extends Component{
           </div>
         </div>
         <div className="recipeBox">
+          <div className="ingredientBox">
+            <div>Ingredients:</div>
+            <ul>
+              {this.createBox(this.props.data[this.props.selIndex]["ingredients"])}
+            </ul>
+          </div>
+          <div className="directionBox">
+            <div>Directions:</div>
+            <ul>
+              {this.createBox(this.props.data[this.props.selIndex]["directions"])}
+            </ul>
+          </div>
         </div>
         <div className="addButton">
           <button id="add-recipe" title="Add Recipe"><i className="far fa-plus-square"></i></button>
